5099a30f9301d745841900f376235d023a6834ee
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2025-05-29 01:00:41 +0900

    impl: start & stop

---
+++ cmd/start.go
@@ -0,0 +1,56 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/inamuu/dok/internal"
+	"github.com/spf13/cobra"
+)
+
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Start a Docker container",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmdContainers := exec.Command("docker", "ps", "-a", "--format", "{{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Status}}\t{{.CreatedAt}}")
+		out, err := cmdContainers.Output()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to get docker containers: %v\n", err)
+			return
+		}
+		lines := strings.Split(strings.TrimSpace(string(out)), "\n")
+		if len(lines) == 0 || (len(lines) == 1 && lines[0] == "") {
+			fmt.Println("No docker containers found.")
+			return
+		}
+		// map: 表示行 -> ID
+		lineToID := make(map[string]string)
+		for _, line := range lines {
+			fields := strings.SplitN(line, "\t", 2)
+			if len(fields) < 2 {
+				continue
+			}
+			id := fields[0]
+			lineToID[line] = id
+		}
+
+		container, err := internal.SelectWithPeco(lines)
+		if err != nil {
+			fmt.Println("peco container select error:", err)
+			return
+		}
+
+		cmdRm := exec.Command("docker", "start", lineToID[container])
+		cmdRm.Stdin = os.Stdin
+		cmdRm.Stdout = os.Stdout
+		cmdRm.Stderr = os.Stderr
+		fmt.Println("Start container:", lineToID[container])
+		cmdRm.Run()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+}
