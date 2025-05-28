73338c2fefc7dbb4d245cc6fe42b74c38a2d7e81
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2025-05-29 01:37:50 +0900

    set version in build script

--- build.sh
+++ build.sh
@@ -9,10 +9,23 @@ GOOS_ARCHES=(
   "darwin arm64"
 )
 
+if [[ $# -ne 1 ]]; then
+  echo "Usage: $0 [version, e.g. v0.1.0]"
+  exit 1
+fi
+
+VERSION=$1
+
+mkdir -p dist/$1
+
+
 for combo in "${GOOS_ARCHES[@]}"; do
   read -r GOOS GOARCH <<< "$combo"
   out="dok-${GOOS}-${GOARCH}"
   [[ "$GOOS" == "windows" ]] && out="${out}.exe"
   echo "Building $out..."
-  GOOS=$GOOS GOARCH=$GOARCH go build -o "dist/$out" main.go
+  GOOS=$GOOS GOARCH=$GOARCH go build -o "dist/$1/$out" main.go
 done
+
+ghr $1 dist/$1
+echo "Release $VERSION created successfully."
